52ffabd8259bf99ff5c0cb9c08219cd68d19e62a
DitijTanwar29 <ditijtanwar2001@gmail.com>
2024-07-13 15:56:20 +0530

    job dropdown added with updates in dashboard and varoous other places

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://surucucvbackend.onrender.com/api/v1";
+export const BASE_URL = "https://surucucvbackend.onrender.com/api/v1";
 // export const BASE_URL = process.env.REACT_APP_BASE_URL
 
 //AUTH ENDPOINTS
