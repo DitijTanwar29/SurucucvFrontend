1bd8eecc00f2fc46cf2e4a1b84233f25a78dedb2
DitijTanwar29 <ditijtanwar2001@gmail.com>
2025-02-20 20:27:42 +0530

    minor fix in apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -3,7 +3,7 @@
 // export const BASE_URL = process.env.REACT_APP_BASE_URL
 // export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const BASE_URL = "http://147.79.101.184/api/v1"
+export const BASE_URL = "http://surucucv.com/api/v1"
 
 
 //AUTH ENDPOINTS
